4b34f6e1183c186e23d1098bf7ba4bea798f5afe
davidojo1144 <davco1144@gmail.com>
2025-05-18 07:33:24 +0100

    Tech Giant Africa

--- Tech_giant_africa/src/components/ResponsiveDisplay.jsx
+++ Tech_giant_africa/src/components/ResponsiveDisplay.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import { AnimatePresence,motion } from 'framer-motion'
 
-const ResponsiveDisplay = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+const ResponsiveDisplay = ({img}) => {
+
+
+  return <AnimatePresence>
+        { img && (
+            <motion.div
+            initial={{ opacity:0, y: -100 }}
+            animate={{ opacity:1, y: 0 }}
+            exit={{ opacity:0, y: -100 }}
+            transition={{duration: 0.3}}
+            >
+                <div className='bg-gray-200  text-gray-600 hover:text-gray-700 cursor-pointer text-md font-semibold uppercase  mt-5 rounded-2xl md:hidden'>
+                    <ul className='flex flex-col items-start p-9 justify-center  gap-7'>
+                        <div className='flex flex-col gap-10'>
+                           <p>About Us</p>
+                           <p>Services Offered</p>
+                           <p>Contact Us</p>
+                        </div>
+                    </ul>
+                </div>
+            </motion.div>
+            )
+        }
+</AnimatePresence>
 }
 
 export default ResponsiveDisplay
--- Tech_giant_africa/src/components/ResponsiveNavbar.jsx
+++ Tech_giant_africa/src/components/ResponsiveNavbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import ResponsiveDisplay from './ResponsiveDisplay'
 
+
 const ResponsiveNavbar = () => {
 
     const [img, setImg] = useState(false)
