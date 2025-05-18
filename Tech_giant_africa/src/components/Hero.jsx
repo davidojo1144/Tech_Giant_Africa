0f86a5520d40beaa2e8661b6fbc17ed6198380a8
davidojo1144 <davco1144@gmail.com>
2025-05-18 08:17:21 +0100

    Tech Giant Africa

--- Tech_giant_africa/src/components/Hero.jsx
+++ Tech_giant_africa/src/components/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
             <div className='text-white md:space-y-8 space-y-3'>
                 <h1 className='prata-regular md:text-4xl text-2xl '>Affordable Websites & Apps for Your Business & Personal Needs.</h1>
                 <p className='md:text-xl text-md'>Building innovative digital solutions at prices that suit your budget.</p>
-                <div className='flex gap-5'>
-                    <button className='py-2 px-5 bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 rounded'>Get a Free Quote</button>
-                    <button className='py-2 px-5 bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 rounded'>View Our Portfolio</button>
+                <div className='flex md:gap-5 gap-3'>
+                    <button className='py-2 px-5 bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 rounded md:text-xl text-xs'>Get a Free Quote</button>
+                    <button className='py-2 px-5 bg-gray-200 hover:bg-gray-300 text-gray-700 hover:text-gray-800 rounded md:text-xl text-xs'>View Our Portfolio</button>
                 </div>
             </div>
-            <img className='md:w-[40%] rounded' src={assets.hero} alt="" />
+            <img className='md:w-[50%]  rounded' src={assets.hero} alt="" />
         </div>
     </div>
     </>
