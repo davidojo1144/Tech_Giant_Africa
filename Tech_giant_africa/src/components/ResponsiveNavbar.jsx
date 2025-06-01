8dedb0014819abbed1c22a144b732ab89b98dee4
davidojo1144 <davco1144@gmail.com>
2025-06-01 11:48:40 +0100

    Tech Giant Africa

--- Tech_giant_africa/src/components/ResponsiveNavbar.jsx
+++ Tech_giant_africa/src/components/ResponsiveNavbar.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
+import UseAnimations from "react-useanimations"
+import menuV2 from 'react-useanimations/lib/menu2'
 import { assets } from '../assets/assets'
 import ResponsiveDisplay from './ResponsiveDisplay'
 
-
 const ResponsiveNavbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
 
-    const [img, setImg] = useState(false)
-
-
-  return (
-    <div className='container'>
-      <div className='flex items-center justify-between md:hidden pt-5'>
-        <img className='w-8' onClick={()=> setImg(!img)} src={img ? assets.cancel : assets.menu} alt="" />
-        <img className='w-16' src={assets.logo} alt="" />
-      </div>
-      <ResponsiveDisplay img={img}/>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='flex items-center justify-between md:hidden pt-5'>
+                <UseAnimations 
+                    animation={menuV2}
+                    size={32}
+                    reverse={menuOpen}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    strokeColor="#FFFFFF" 
+                    speed={1.5}
+                    className="cursor-pointer" 
+                />
+                <img className='w-16' src={assets.logo} alt="Logo" />
+            </div>
+            <ResponsiveDisplay img={menuOpen}/>
+        </div>
+    )
 }
 
 export default ResponsiveNavbar
